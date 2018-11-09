cc346396edec46083fb735ac113968627836e47d
hoyoho <474844988@qq.com>
2018-11-09 14:38:45 +0800

    Create monitor.h

---
+++ monitor.h
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+#include <csignal>
+#include <netinet/ip.h>
+#include <sys/time.h>
+#include <net/ethernet.h>
+#include <pthread.h>
+#include <pcap.h>
+#include <libnet.h>
+
+//端口选择
+//监控选择
+    //方向
+    //函数
+    //流水
+    //序列
+    //跳跃
+        //时间
+        //包数
+//功能选择
+    //丢包
+    //编辑
+    //延时
+class monitor
+{
+    private:
+        
+    public:
+        monitor();
+        ~monitor();
+
+        
+};

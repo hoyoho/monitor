25537063cadce17266b1c77643feb391ca361967
hoyoho <474844988@qq.com>
2018-11-13 10:23:23 +0800

    Update commandparser.h

--- commandparser.h
+++ commandparser.h
@@ -1,3 +1,6 @@
+#ifndef COMMAND_PARSER_H
+#define COMMAND_PARSER_H
+
 #include <string>
 #include <vector>
 #include "parameter.h"
@@ -5,11 +8,12 @@
 class CommandParser
 {
     private:
+        
         enum {ASK_HELP = 0};
         unsigned int stageCount;
-        std::vector<Parameter> vParameter; 
+        std::vector<Parameter> vParameter;
         void printInfo(int info);
-        int createStagePara(string cmd);
+        int createStagePara(const std::vector<std::string>& para);
     public:
         
         CommandParser(int argc, char* argv[]);
@@ -17,3 +21,6 @@ class CommandParser
         unsigned int getStageCount();
         std::vector<Parameter> vStageParameter;
 };
+
+#endif
+

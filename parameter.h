7cc8dfc5133eabc086dc6a6a2f31a6b4a57ae290
hoyoho <474844988@qq.com>
2018-11-13 10:21:40 +0800

    Update parameter.h

--- parameter.h
+++ parameter.h
@@ -6,12 +6,13 @@
 class Parameter
 {
     private:
-        const int MAX_INTERFACE_LEN = 40;
+        const static int MAX_INTERFACE_LEN = 40;
     public:
         unsigned int    flag;
-        char            src[MAX_INTERFACE_LEN];        
+        char            src[MAX_INTERFACE_LEN];
         char            dst[MAX_INTERFACE_LEN];
         unsigned int    gap;
-        std::vector<unsigned int> choSequence;   
+        unsigned int    stageId;
+        std::vector<unsigned int> choSequence;
 };
 

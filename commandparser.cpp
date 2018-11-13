42793f7fa4d1de600af63bd98e7962ce93219f8c
hoyoho <474844988@qq.com>
2018-11-13 17:18:55 +0800

    Update commandparser.cpp

--- commandparser.cpp
+++ commandparser.cpp
@@ -3,6 +3,7 @@
 #include <cstring>
 #include <iostream>
 #include <string>
+#include <sstream>
 #include <vector>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/stdout_color_sinks.h>
@@ -51,49 +52,66 @@ CommandParser::CommandParser(int argc, char* argv[])
         printInfo(ASK_HELP);
         return;
     }
-    auto it = command.begin() + 1;
-    if(*it != "-I")
+    auto itLast = command.begin();
+    decltype(itLast) itCurrent = itLast;
+    while(*itLast != "-I") 
     {
-        //这里可以根据用户已经输入的参数多少
-        //提供更加精准的帮助内容
-        printInfo(ASK_HELP);
-        return;
+        ++itLast;
+        if(itLast == command.end())
+        {
+            //这里可以根据用户已经输入的参数多少
+            //提供更加精准的帮助内容
+            printInfo(ASK_HELP);
+            return;
+        }
     }
-    createStagePara(vector<string>(it, it + 2));
+    while(*itCurrent != "-L") 
+    {
+        ++itCurrent;
+        if(itCurrent == command.end())
+        {
+            //这里可以根据用户已经输入的参数多少
+            //提供更加精准的帮助内容
+            printInfo(ASK_HELP);
+            return;
+        }
+    }
+    if(createInterfacePara(vector<string>(itLast, itCurrent)) != true)
+    {
+        exit(0);
+    }
 
 
     //提取stage
-    if(command.size() < 5)//monitor  -I interface,interface -L 1
-    {
-        //这里可以根据用户已经输入的参数多少
-        //提供更加精准的帮助内容
-        printInfo(ASK_HELP);
-        return;
-    }
-    it = command.begin() + 3;
-    if(*it != "-L")
-    {
-        //这里可以根据用户已经输入的参数多少
-        //提供更加精准的帮助内容
-        printInfo(ASK_HELP);
-        return;
-    }
-    auto tailIt = it + 1;
+    itLast= itCurrent;
+    itCurrent += 1;
     while(true)
     {
-       if(tailIt == command.end())
+       if(itCurrent == command.end())
        {
-            createStagePara(vector<string>(it, tailIt));
+            createStagePara(vector<string>(itLast, itCurrent));
             break;
        }
-       if(*tailIt == "-L")
+       if(*itCurrent == "-L")
        {
-            createStagePara(vector<string>(it, tailIt));
-            it = tailIt;
+            createStagePara(vector<string>(itLast, itCurrent));
+            itLast = itCurrent;
        }
-       ++tailIt;
+       ++itCurrent;
     }
 }
+/*------------------------------------------------------
+const unsigned NEGITIVE     =   0x0001;
+const unsigned PKT_COUNT    =   0x0002;
+const unsigned GAP_VALUE    =   0x0004;
+const unsigned SERIAL_NUM   =   0x0008;
+const unsigned BITMAP         =   0x0010;
+const unsigned FUNCTION     =   0x0020;
+const unsigned PKT_DROP     =   0x0040;
+const unsigned PKT_REPEAT   =   0x0080;
+const unsigned PKT_DELAY    =   0x0100;
+const unsigned PROTOCOL     =   0x0200;
+
 
 
 class Parameter
@@ -101,41 +119,90 @@ class Parameter
     private:
         const static int MAX_INTERFACE_LEN = 40;
     public:
+
+        DirectionFlag flag;
         unsigned int    flag;
         char            src[MAX_INTERFACE_LEN];
         char            dst[MAX_INTERFACE_LEN];
         unsigned int    gap;
         unsigned int    stageId;
         std::vector<unsigned int> choSequence;
-        bool validity;
-        
 };
 
-int CommandParser::createStagePara(const vector<string>& paraCmd)
-{
+-------------------------------------------------------*/
 
+std::vector<std::string>  CommandParser::split(std::string s, char delim)
+{
+    std::vector<std::string> elems;
+    if(s.empty())
+        return elems;
+    std::stringstream ss(s);
+    std::string item;
+ 
+    while (std::getline(ss, item, delim)) 
+    {
+        elems.push_back(item);
+    }
+    return elems;
+}
+
+bool CommandParser::createInterfacePara(const vector<string>& paraCmd)
+{
     auto console = Logger::getLogger();
     Parameter* pPara = new Parameter();
-    if(paraCmd[0] == "-I")
+    //此处应该添加错误校验
+    std::vector<std::string> temp; 
+    string tempItrfc;
+    for(auto it = paraCmd.begin() + 1; it != paraCmd.end(); ++it)
     {
-        strcpy(pPara->src, strtok(paraCmd.c_str(), ", "));
-        strcpy(pPara->dst, strtok(NULL, paraCmd.c_str()));
-        pPara->stageId = 0;
-        vParameter.push_back(*pPara);
+        tempItrfc += *it;
     }
-    if(paraCmd[0] == "-L")
+    temp = split(tempItrfc, ',');
+    if(temp.size() < 2)
     {
-        pPara->stageId = 0;
-        vParameter.push_back(*pPara);
+        return false;
     }
+    pPara->src = temp[0];
+    pPara->dst = temp[1];
+    pPara->stageId = 0;
+    vParameter.push_back(*pPara);
+    return true;
 
+}
 
-
-    for(auto it = para.begin(); it != para.end(); ++it)
+bool CommandParser::createStagePara(const vector<string>& paraCmd)
+{
+    auto console = Logger::getLogger();
+    Parameter* pPara = new Parameter();
+    //此处应该添加错误校验
+    pPara -> stageId = atoi(optarg);
+    auto it = paraCmd.begin();
+    auto tailIt = it + 1;
+    while(true)
     {
-        console->debug(*it);
+       if(tailIt == paraCmd.end())
+       {
+            //处理从开始到结尾的所有
+            for(auto itDebug = it; itDebug != tailIt; it++)
+                console->debug(*it);
+            break;
+       }
+       if(*tailIt == "-N")
+       {
+            //处理两次
+            for(decltype(it) itDebug = it; itDebug != tailIt; it++)
+                console->debug(*it);
+            it = tailIt;
+            tailIt = paraCmd.end();
+            console->debug("again");
+            for(decltype(it) itDebug = it; itDebug != tailIt; it++)
+                console->debug(*it);
+       }
+       ++tailIt;
     }
-    return 0;
+    pPara->stageId = 0;
+    vParameter.push_back(*pPara);
+    return true;
 }
 
 void CommandParser::printInfo(int info)

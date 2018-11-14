1a56c029f518f5d27f47a17b0baa537f00948473
hoyoho <474844988@qq.com>
2018-11-14 17:06:16 +0800

    Update parameter.h

--- parameter.h
+++ parameter.h
@@ -5,17 +5,16 @@
 /*----------------------------------------------
 
 ----------------------------------------------- */
-const unsigned POSITIVE     =   0x00000001;
-const unsigned NEGITIVE     =   0x00000002;
-const unsigned PKT_COUNT    =   0x00000004;
-const unsigned GAP_VALUE    =   0x00000008;
-const unsigned SERIAL_NUM   =   0x00000010;
-const unsigned BITMAP       =   0x00000020;
-const unsigned FUNCTION     =   0x00000040;
-const unsigned PKT_DROP     =   0x00000080;
-const unsigned PKT_REPEAT   =   0x00000100;
-const unsigned PKT_DELAY    =   0x00000200;
-const unsigned OBJECT       =   0x00000400;
+const unsigned PKT_COUNT    =   0x00000001;
+const unsigned GAP_VALUE    =   0x00000002;
+const unsigned SEQUENCE_NUM   =   0x00000004;
+const unsigned BITMAP       =   0x00000008;
+const unsigned FUNCTION     =   0x00000010;
+const unsigned PKT_DROP     =   0x00000020;
+const unsigned PKT_REPEAT   =   0x00000040;
+const unsigned PKT_DELAY    =   0x00000080;
+const unsigned OBJECT       =   0x00000100;
+
 
 
 struct TcpHead
